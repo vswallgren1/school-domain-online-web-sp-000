d3611593f1e608ffa074eb7c85aa0755b9196db3
Vince W <vincewallgren@me.com>
2021-03-24 04:57:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,13 +9,13 @@ class School
     @roster
   end
 
-  def add_student=(student_name, grade)
-    @student_name = student_name
+  def add_student=(name, grade)
+    @student_name = name
     @grade = grade
     if @roster.include?(grade) == false
       @roster[grade] = []
     end
-    @roster[grade] << student_name
+    @roster[grade] << name
   end
 
   def grade(number)
@@ -23,7 +23,7 @@ class School
   end
 
   def sort
-    @roster.each do |grade, student_name|
+    @roster.each do |grade, name|
       @roster[grade] = name.sort
     end
   end
